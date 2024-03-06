968e5b2a9e70380d1b5f292f14dbabe3f86dd6c9
alkuzin <alkuzindev@gmail.com>
2024-03-06 16:32:23 +0300

    feat: added vga colors

--- kernel/include/kernel/vga.h
+++ kernel/include/kernel/vga.h
@@ -25,10 +25,33 @@ SOFTWARE.
 #ifndef _KERNEL_VGA_H_
 #define _KERNEL_VGA_H_ 
 
-#define VIDEO_MEMORY   0xB8000
+#define VIDEO_MEMORY       0xB8000
 #define VGA_SCREEN_WIDTH   80
 #define VGA_SCREEN_HEIGHT  25
 
+#include <libk/stdint.h>
 
+enum vga_color {
+	VGA_COLOR_BLACK,
+	VGA_COLOR_BLUE,
+	VGA_COLOR_GREEN,
+	VGA_COLOR_CYAN,
+	VGA_COLOR_RED,
+	VGA_COLOR_MAGENTA,
+	VGA_COLOR_BROWN,
+	VGA_COLOR_LIGHT_GREY,
+	VGA_COLOR_DARK_GREY,
+	VGA_COLOR_LIGHT_BLUE,
+	VGA_COLOR_LIGHT_GREEN,
+	VGA_COLOR_LIGHT_CYAN,
+	VGA_COLOR_LIGHT_RED,
+	VGA_COLOR_LIGHT_MAGENTA,
+	VGA_COLOR_LIGHT_BROWN,
+	VGA_COLOR_WHITE
+};
+
+uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg);
+
+uint16_t vga_entry(unsigned char uc, uint8_t color);
 
 #endif /* _KERNEL_VGA_H_ */
