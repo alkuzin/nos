287e1a10d68d692b45ba924fe32b82b98a8425fa
alkuzin <alkuzindev@gmail.com>
2024-03-09 14:01:06 +0300

    feat: define a signed integer types

--- kernel/include/libk/stdint.h
+++ kernel/include/libk/stdint.h
@@ -30,9 +30,19 @@ typedef unsigned int    uint32_t;
 typedef unsigned short  uint16_t;
 typedef unsigned char   uint8_t;
 
+typedef long            int64_t;
+typedef int             int32_t;
+typedef short           int16_t;
+typedef char            int8_t;
+
 typedef unsigned long   u64;
 typedef unsigned int    u32;
 typedef unsigned short  u16;
 typedef unsigned char   u8;
+
+typedef long            i64;
+typedef int             i32;
+typedef short           i16;
+typedef char            i8;
 
 #endif /* _LIBK_STDINT_H_ */
