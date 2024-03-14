43cf38e74c5f677967b88598688c0b5821a41c87
alkuzin <alkuzindev@gmail.com>
2024-03-14 16:11:08 +0300

    feat: added Programmable Interval Timer

---
+++ kernel/timer.c
@@ -0,0 +1,59 @@
+/*
+MIT License
+
+Copyright (c) 2024 Alexander (@alkuzin)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <kernel/timer.h>
+#include <kernel/ports.h>
+#include <libk/stdint.h>
+#include <kernel/tty.h>
+#include <kernel/irq.h>
+
+
+uint64_t ticks;
+const uint32_t freq = 100;
+
+
+void on_irq0(int_reg_t *regs)
+{
+    if(regs->int_no < 32)
+        kpanic("%s\n", "on_irq0 error"); 
+    
+    ticks++;
+}
+
+void timer_init(void)
+{
+    uint32_t divisor;
+
+    ticks = 0;
+    irq_install_handler(0, &on_irq0);
+
+    divisor = 1193180 / freq; /* MHz */
+
+    out_port_b(0x43, 0x36);
+
+    __asm__ volatile("cli");
+
+    out_port_b(0x40, (uint8_t)(divisor & 0xFF));
+    out_port_b(0x40, (uint8_t)((divisor >> 8) & 0xFF));
+}
