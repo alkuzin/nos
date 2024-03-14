5a599d917e15646fdbc688a1744d913ee0b02088
alkuzin <alkuzindev@gmail.com>
2024-03-14 09:32:06 +0300

    feat: added Interrupt Requests

---
+++ kernel/irq.c
@@ -0,0 +1,97 @@
+/*
+MIT License
+
+Copyright (c) 2024 Alexander (@alkuzin)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <kernel/ports.h>
+#include <kernel/tty.h>
+#include <kernel/irq.h>
+
+
+char *exception_msgs[] = {
+    "division by zero",
+    "debug",
+    "non maskable interrupt",
+    "breakpoint",
+    "into detected overflow",
+    "out of bounds",
+    "invalid opcode",
+    "no coprocessor",
+    "double fault",
+    "coprocessor segment overrun",
+    "bad TSS",
+    "segment not present",
+    "stack fault",
+    "general protection fault",
+    "page fault",
+    "unknown interrupt",
+    "coprocessor fault",
+    "alignment fault",
+    "machine check",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved"
+};
+
+void isr_handler(int_reg_t *regs)
+{
+    if(regs->int_no < 32)
+        kpanic(" ((%s))\n", exception_msgs[regs->int_no]);
+}
+
+static void *irq_routines[16] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0 
+};
+
+void irq_install_handler(int irq, void (*handler)(int_reg_t *r)) {
+    irq_routines[irq] = handler;
+}
+
+void irq_uninstall_handler(int irq) {
+    irq_routines[irq] = 0;
+}
+
+void irq_handler(int_reg_t *regs)
+{
+    void (*handler)(int_reg_t *regs);
+
+    handler = irq_routines[regs->int_no - 32];
+
+    if(handler)
+        handler(regs);
+
+    if(regs->int_no >= 40)
+        out_port_b(0xA0, 0x20);
+
+    out_port_b(0x20, 0x20);
+}
