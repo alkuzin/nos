0b9c899538b7bf38e26228b310e11f12aeecea60
alkuzin <alkuzindev@gmail.com>
2024-03-20 12:32:40 +0300

    fix: fixed issue with copying last character

--- kernel/libk/string.c
+++ kernel/libk/string.c
@@ -53,7 +53,7 @@ size_t strlcpy(char *dest, const char *src, size_t size)
     size_t i;
 
     i = 0;
-    while(src[i] && i < size - 1) {
+    while(src[i] && i < size) {
         dest[i] = src[i];
         i++;
     }
