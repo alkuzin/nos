f4a379cc7e98c112a1cb6fa36a697e573f1c7500
alkuzin <alkuzindev@gmail.com>
2024-05-16 21:18:02 +0300

    refactor: added separate makefiles for common entities and switched project to new C23 standard

---
+++ libc/libc_common.mk
@@ -0,0 +1,9 @@
+CC = gcc
+
+INCLUDE_DIR = ../../include/
+LIBC_DIR    = $(INCLUDE_DIR)/libc
+
+LFLAGS = -m elf_i386 -relocatable -z noexecstack 
+CFLAGS = -Wall -Wextra -Werror -g -std=c2x -m32 -O2 -Oz -nostdlib -nodefaultlibs \
+         -fno-pie -fno-exceptions -fno-stack-protector -fno-builtin  \
+         -ffreestanding -I $(INCLUDE_DIR) -I $(LIBC_DIR)
