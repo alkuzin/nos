2eb455567a8c4967dfba9e1269783e11c4ace4fd
alkuzin <alkuzindev@gmail.com>
2024-09-16 13:16:56 +0300

    refactor: switched to CMake

--- lib/common.mk
+++
@@ -1,6 +0,0 @@
-INCLUDE_DIR = ../../include/
-CC          = gcc
-LFLAGS      = -m elf_i386 -relocatable -z noexecstack 
-CFLAGS      = -Wall -Wextra -Werror -g -std=c2x -m32 -O2 -Oz -nostdlib -nodefaultlibs \
-              -fno-pie -fno-exceptions -fno-stack-protector -fno-builtin  \
-              -ffreestanding -I $(INCLUDE_DIR)
