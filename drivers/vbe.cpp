37ecc06e9c7d49db7449fcf8f2ad7b3eca09da21
alkuzin <alkuzindev@gmail.com>
2024-09-20 16:09:58 +0300

    refactor: removed extra function

--- drivers/vbe.cpp
+++ drivers/vbe.cpp
@@ -18,7 +18,6 @@
 
 #include <kernel/arch/x86/io.hpp>
 #include <kernel/drivers/vbe.hpp>
-#include <kernel/gfx/gfx.hpp>
 
 
 namespace kernel {
@@ -37,38 +36,13 @@ static constexpr inline void write(u16 index, u16 data) noexcept
     arch::x86::outw(VBE_DISPI_IOPORT_DATA, data);
 }
 
-/**
- * @brief Set VBE video mode.
- * 
- * @param [in] width - given screen width.
- * @param [in] height - given screen height.
- * @param [in] depth - given number of bits per pixel.
- * @param [in] pitch - given number of bytes per scanline.
- * @param [in] fb_addr - given framebuffer pointer.
- */
-static constexpr void set_video_mode(u16 width, u16 height, u16 depth, u16 pitch, u32 *fb_addr) noexcept
+void init(const multiboot_info_t& mboot) noexcept
 {
-    gfx::set_framebuffer(fb_addr);
-    gfx::set_height(height);
-    gfx::set_width(width);
-    gfx::set_pitch(pitch);
-
     write(0x00, 0x4F02); // set VBE mode
-    write(0x01, width);
-    write(0x02, height);
-    write(0x03, depth);
+    write(0x01, mboot.framebuffer_width);
+    write(0x02, mboot.framebuffer_height);
+    write(0x03, mboot.framebuffer_bpp);
     write(0x04, VBE_DISPI_ENABLED | VBE_DISPI_LFB_ENABLED); // enable LFB mode
-}
-
-void init(const multiboot_info_t& mboot)
-{
-    set_video_mode(
-        mboot.framebuffer_width,
-        mboot.framebuffer_height,
-        mboot.framebuffer_bpp,
-        mboot.framebuffer_pitch,
-        reinterpret_cast<u32*>(mboot.framebuffer_addr)
-    );
 }
 
 } // namespace vbe
