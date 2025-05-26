33273db395bbef79d22579a47eee4443e583ba1a
alkuzin <alkuzindev@gmail.com>
2025-05-26 22:55:41 +0300

    [feat]: displayed CPU info during kernel initialization

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -7,14 +7,43 @@
 
 pub mod gfx;
 
-use crate::{log, multiboot::MultibootInfo};
+use crate::{log, multiboot::MultibootInfo, hal};
+use core::str;
+
+/// Display CPU related info.
+fn display_cpu_info() {
+    // Display basic CPU info.
+    let cpu = hal::cpu::get_cpu_info();
+    let vendor_id = str::from_utf8(&cpu.vendor_id).unwrap();
+    let brand = str::from_utf8(&cpu.brand).unwrap();
+
+    log::info!("Found CPU:");
+    log::info!("CPU vendor: {}", vendor_id);
+    log::info!("CPU brand: {}", brand);
+    log::info!("CPU family: {}", cpu.family);
+    log::info!("CPU model: {}", cpu.model);
+    log::info!("CPU max CPUID: {}", cpu.max_cpuid);
+
+    // Display virtualization extension info.
+    if hal::cpu::is_support_virtualization() {
+        log::info!("Virtualization is supported");
+    }
+    else {
+        log::info!("Virtualization is not supported");
+    }
+
+    // Display hypervisor info.
+    if let Some(hypervisor_id) = hal::cpu::get_hypervisor_id() {
+        let id = str::from_utf8(&hypervisor_id).unwrap();
+        log::info!("Running under hypervisor: {}", id);
+    }
+}
 
 /// Initialize kernel.
 ///
 /// # Parameters
 /// - `boot_info` - given multiboot info structure.
 pub fn init(boot_info: &MultibootInfo) {
-    // Initialize kernel logger.
     log::init_serial_writer();
     log::success!("Initialized kernel serial logger");
 
@@ -24,6 +53,5 @@ pub fn init(boot_info: &MultibootInfo) {
     log::init_terminal_writer(fb.clone());
     log::success!("Initialized kernel terminal logger");
 
-    // TODO: implement CPU info struct.
-    // TODO: display CPU info + check whether OS running under hypervisor.
+    display_cpu_info();
 }
