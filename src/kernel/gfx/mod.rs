c38b5e49e1dc6c50073565aca007f22513969eb0
alkuzin <alkuzindev@gmail.com>
2025-05-26 12:59:42 +0300

    [feat]: added kernel builtin font

--- src/kernel/gfx/mod.rs
+++ src/kernel/gfx/mod.rs
@@ -5,7 +5,9 @@
 
 //! Kernel graphics subsystem.
 
-use crate::{log, multiboot::MultibootInfo, drivers::vbe::Framebuffer};
+mod font;
+
+use crate::{drivers::vbe::Framebuffer, log, multiboot::MultibootInfo};
 use core::ptr;
 
 /// RGB color type.
@@ -27,12 +29,12 @@ macro_rules! rgb {
 /// Standard color enumeration.
 #[repr(u32)]
 pub enum Color {
-    White  = rgb!(0xFF, 0xFF, 0xFF),
-    Black  = rgb!(0x00, 0x00, 0x00),
-    Red    = rgb!(0xFF, 0x00, 0x00),
-    Green  = rgb!(0x00, 0xFF, 0x00),
-    Blue   = rgb!(0x00, 0x00, 0xFF),
-    Gray   = rgb!(0xBF, 0xBF, 0xBF),
+    White = rgb!(0xFF, 0xFF, 0xFF),
+    Black = rgb!(0x00, 0x00, 0x00),
+    Red = rgb!(0xFF, 0x00, 0x00),
+    Green = rgb!(0x00, 0xFF, 0x00),
+    Blue = rgb!(0x00, 0x00, 0xFF),
+    Gray = rgb!(0xBF, 0xBF, 0xBF),
     Yellow = rgb!(0xFF, 0xFF, 0x44),
     Orange = rgb!(0xFF, 0x70, 0x00),
 }
@@ -46,11 +48,11 @@ pub enum Color {
 /// - Framebuffer info struct.
 fn get_framebuffer(boot_info: &MultibootInfo) -> Framebuffer {
     Framebuffer {
-        addr:   boot_info.framebuffer_addr as u32,
-        pitch:  boot_info.framebuffer_pitch,
-        width:  boot_info.framebuffer_width,
+        addr: boot_info.framebuffer_addr as u32,
+        pitch: boot_info.framebuffer_pitch,
+        width: boot_info.framebuffer_width,
         height: boot_info.framebuffer_height,
-        bpp:    boot_info.framebuffer_bpp,
+        bpp: boot_info.framebuffer_bpp,
     }
 }
 
@@ -78,13 +80,14 @@ pub unsafe fn put_pixel(fb: &Framebuffer, x: u32, y: u32, color: Rgb) {
 /// Fill screen with specific color.
 ///
 /// # Parameters
+/// - `fb`    - given framebuffer info struct.
 /// - `color` - given color to fill with.
 pub fn fill_screen(fb: &Framebuffer, color: Rgb) {
     let total_pixels = (fb.width * fb.height) as usize;
 
     unsafe {
         let buffer = fb.addr();
-        let mut i  = 0;
+        let mut i = 0;
 
         // Filling 4 pixels at once.
         while i <= total_pixels - 4 {
@@ -98,6 +101,53 @@ pub fn fill_screen(fb: &Framebuffer, color: Rgb) {
         // Color any remaining pixels.
         for j in i..total_pixels {
             ptr::write(buffer.wrapping_add(j), color);
+        }
+    }
+}
+
+/// Draw font character on the screen.
+///
+/// # Parameters
+/// - `fb`    - given framebuffer info struct.
+/// - `c`     - given character to print.
+/// - `x`     - given x pixel position.
+/// - `y`     - given y pixel position.
+/// - `fg`    - given foreground color.
+/// - `bg`    - given background color.
+/// - `is_bg` - given param determine whether to display the `bg`.
+pub fn draw_char(
+    fb: &Framebuffer,
+    c: char,
+    x: usize,
+    y: usize,
+    fg: Rgb,
+    bg: Rgb,
+    is_bg: bool,
+) {
+    static MASK: [u8; 8] = [128, 64, 32, 16, 8, 4, 2, 1];
+    let font_ptr = font::FONT.as_ptr();
+    let glyph: *const u8 =
+        font_ptr.wrapping_add(c as usize * font::CHAR_HEIGHT);
+
+    unsafe {
+        let buffer = fb.addr();
+
+        for cy in 0..font::CHAR_HEIGHT {
+            // Read the entire row of pixels at once.
+            let pixel = *glyph.wrapping_add(cy);
+
+            for (cx, &mask) in MASK.iter().enumerate().take(font::CHAR_WIDTH) {
+                let color = if pixel & mask != 0 {
+                    fg
+                } else if is_bg {
+                    bg
+                } else {
+                    continue;
+                };
+
+                let pos = (y + cy) * fb.width as usize + (x + cx);
+                ptr::write(buffer.wrapping_add(pos), color);
+            }
         }
     }
 }
